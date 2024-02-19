edae78b71cc5872ba8d1572748e184b0735a05c6
Mathilde <make0003@stud.kea.dk>
2024-02-19 13:42:48 +0100

    d.19/02

--- js/shrubs_trees.js
+++ js/shrubs_trees.js
@@ -1,6 +1,8 @@
 //https://lxnfykqvjokljzdduzaq.supabase.co
 
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4bmZ5a3F2am9rbGp6ZGR1emFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDg2MzksImV4cCI6MjAyMzQ4NDYzOX0.BWTCFL4NjwtJLRusGsYDCJOh0go7Wwpi1l7ouN_bEnE
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
 
 fetch("https://lxnfykqvjokljzdduzaq.supabase.co/rest/v1/vild_mad?limit=22", {
   method: "GET",
