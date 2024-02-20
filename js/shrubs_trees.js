b2045353f9b92b856f01b4059882ba2ace198000
Mathilde <make0003@stud.kea.dk>
2024-02-20 16:18:32 +0100

    overskrifter - sæsoner

--- js/shrubs_trees.js
+++ js/shrubs_trees.js
@@ -2,9 +2,12 @@
 
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4bmZ5a3F2am9rbGp6ZGR1emFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDg2MzksImV4cCI6MjAyMzQ4NDYzOX0.BWTCFL4NjwtJLRusGsYDCJOh0go7Wwpi1l7ouN_bEnE
 const urlParams = new URLSearchParams(window.location.search);
-const season = urlParams.get("season");
-if (season) {
-  fetch(`https://lxnfykqvjokljzdduzaq.supabase.co/rest/v1/vild_mad?season=eq.${season}`, {
+const mySeason = urlParams.get("season");
+
+document.querySelector(".season_overskrift").textContent = mySeason;
+
+if (mySeason) {
+  fetch(`https://lxnfykqvjokljzdduzaq.supabase.co/rest/v1/vild_mad?season=eq.${mySeason}`, {
     method: "GET",
     headers: {
       apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4bmZ5a3F2am9rbGp6ZGR1emFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDg2MzksImV4cCI6MjAyMzQ4NDYzOX0.BWTCFL4NjwtJLRusGsYDCJOh0go7Wwpi1l7ouN_bEnE",
@@ -44,7 +47,7 @@ function showItem(item) {
 
   copy.querySelector("img").src = item.image;
 
-  copy.querySelector("h2").textContent = item.title;
+  copy.querySelector("h3").textContent = item.title;
   copy.querySelector("a").href = `plant.html/${item.id}`;
 
   //append
