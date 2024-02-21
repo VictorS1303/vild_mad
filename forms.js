d934ab9bd111f3b370e243a3fa0d3a101a819d88
Frederikke¨ <frvo0001@stud.kea.dk>
2024-02-21 12:05:12 +0100

    stuff

---
+++ forms.js
@@ -0,0 +1,23 @@
+function allowDrop(event) {
+    event.preventDefault();
+    // document.getElementById('dropBox').style.borderColor = '#4caf50';
+}
+
+function handleDrop(event) {
+    event.preventDefault();
+    // document.getElementById('dropBox').style.borderColor = '#ccc';
+
+    var files = event.dataTransfer.files;
+    handleFiles(files);
+}
+
+function handleFiles(files) {
+    var fileInput = document.getElementById('fileInput');
+    fileInput.files = files;
+
+    var dragText = document.getElementById('dragText');
+
+    if (files.length > 0) {
+        dragText.innerHTML = "<span>x</span>" +files[0].name;
+    } else {
+        dragText.innerHTML = 'Træk og slip en fil her eller klik for at 
